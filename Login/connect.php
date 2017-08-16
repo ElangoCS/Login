968e19fb78277c8f3f2fcbc9ceee437b9dd80f7c
Elango <elangocandy70@gmail.com>
2017-08-16 12:50:11 +0530

    Add files via upload

---
+++ Login/connect.php
@@ -0,0 +1,10 @@
+<?php
+	
+	
+	$con = mysqli_connect('localhost' , "root", '', 'login1');
+	 if(mysqli_connect_errno())
+	 {
+		 echo "Error occured while connecting with database".mysqli_connect_errno();
+	 }
+	 session_start();
+?>
